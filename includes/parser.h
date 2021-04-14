2d9274d24d53d5f3ab882c250274021d8f5f72d5
hgrampa <hgrampa21@gmail.com>
2021-04-14 16:18:42 +0300

    Add parser prototype

---
+++ includes/parser.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgrampa <hgrampa@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 14:35:39 by hgrampa           #+#    #+#             */
+/*   Updated: 2021/04/14 14:41:57 by hgrampa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# include <unistd.h>
+# include "libft.h"
+
+# define _PRS_DELIMITERS " \t"
+# define _PRS_CONTROLERS ";|<>"
+# define _PRS_BUFFSIZE 32
+
+enum e_parser_st
+{
+	EPS_CORE,
+	EPS_STRQT,
+	EPS_WKQT,
+	EPS_ENV,
+	EPS_ESC,
+};
+
+typedef int (*t_state_body)(char**, t_list**, struct s_pcontext*);
+
+typedef struct s_pcontext
+{
+	t_state_body		previous_state;
+	t_state_body		current_state;
+	char				*buffer;
+	t_list				**words;
+	char				process:1;
+
+};
+
+
+
+int		core_state(char **line, t_list **words, struct s_pcontext *context);
+int		squotes_state(char **line, t_list **words, struct s_pcontext *context);
+int		wquotes_state(char **line, t_list **words, struct s_pcontext *context);
+int		env_state(char **line, t_list **words, struct s_pcontext *context);
+int		esc_state(char **line, t_list **words, struct s_pcontext *context);
+
+int		pbuffer_add_char(struct s_pcontext *context, char c); // если null то открывает
+int		pbuffer_add_str(struct s_pcontext *context, char *str); // если null то открывает
+int		pbuffer_open(struct s_pcontext *context);
+int		pbuffer_close(struct s_pcontext *context);
+
+
+
+#endif
--- sources/parser/parser.c
+++ sources/parser/parser.c
@@ -6,31 +6,145 @@
 /*   By: hgrampa <hgrampa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 13:04:19 by hgrampa           #+#    #+#             */
-/*   Updated: 2021/04/11 15:20:54 by hgrampa          ###   ########.fr       */
+/*   Updated: 2021/04/14 16:18:12 by hgrampa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "parser.h"
 
-enum e_parser_st
+
+
+void	pcontext_set_state(struct s_pcontext *context, t_state_body next_state)
 {
-	PRS_MAIN,
-	partial_quoting,
-	full_quoting,
-};
+	context->previous_state = context->current_state;
+	context->current_state = next_state;
+}
 
-int		core_state(char **line, t_list **words);
-int		strong_quotes_state(char **line, t_list **words);
-int		weak_quotes_state(char **line, t_list **words);
-int		env_state(char **line, t_list **words);
-
-int		parse_line(char **line, t_list **words)
+void	pcontext_return_state(struct s_pcontext *context)
 {
-	
-	if (!ft_list_add(words, ft_strdup("dfsdfsdf")))
+	context->current_state = context->previous_state;
+	context->previous_state = NULL;
+}
+
+void	pcontext_end_process(struct s_pcontext *context)
+{
+	context->process = 0;
+}
+
+int		squotes_state(char **line, t_list **words, struct s_pcontext *context)
+{
+	if (**line == '\'') // по большей части условие бессмысленно
+		*line++;
+	// открываю буфер
+	while (21)
 	{
-		return (0);
+		if (**line == '\0')
+		{
+			pcontext_end_process(context);
+			return (1); // TODO или сразу тут ошибку сделать
+		}
+		else if (line == '\'')
+		{
+			// ! не закрываю буфер
+			pcontext_return_state(context);
+			return (1);
+		}
+		// добавляю символ к буферу
+		*line++;
 	}
+}
 
+int		wquotes_state(char **line, t_list **words, struct s_pcontext *context)
+{
+	if (**line == '\"') // по большей части условие бессмысленно
+		*line++;
+	// открываю буфер
+	while (21)
+	{
+		if (**line == '\0')
+		{
+			pcontext_end_process(context);
+			return (1); // TODO или сразу тут ошибку сделать
+		}
+		else if (line == '\"')
+		{
+			// ! не закрываю буфер
+			pcontext_return_state(context);
+			return (1);
+		}
+		// добавляю символ к буферу
+		*line++;
+	}	
+}
+
+int		core_state(char **line, t_list **words, struct s_pcontext *context)
+{
+	while (21)
+	{
+		if (**line == '\0')
+		{
+			pcontext_end_process(context);
+			return (1); // TODO вероятно можно что-то лучше придумать
+		}
+		else if (**line == '$')
+		{
+			pcontext_set_state(context, env_state);
+			return (1);
+		}
+		else if (**line == '\"')
+		{
+			pcontext_set_state(context, wquotes_state);
+			return (1);
+		}
+		else if (**line == '\'')
+		{
+			pcontext_set_state(context, squotes_state);
+			return (1);
+		}
+		else if (**line == '\\')
+		{
+			pcontext_set_state(context, esc_state);
+			return (1);
+		}
+		else if (ft_strchr(_PRS_CONTROLERS, **line) != NULL)
+		{
+			// закрываю буфер и записываю слово
+			// добавляю симол к новому слову
+			// ! если это что то вроде >> - должно остаться одним словом
+			// Вероятно я добавлю еще одну state
+		}
+		else if (ft_strchr(_PRS_DELIMITERS, **line) != NULL)
+		{
+			// закрываю буфер и записываю слово
+			// проматываю символ
+		}
+		// добавляю символ к буферу
+		*line++;
+	}
 	return (1);
+}
+
+
+
+int		parse_line(char *line, t_list **words)
+{
+	int					result;
+	struct s_pcontext	context;
+
+	context.previous_state = NULL;
+	context.current_state = core_state;
+	context.buffer = NULL;
+	context.process = 1;
+
+	while (context.process)
+	{
+		result = context.current_state(&line, words, &context);
+		if (result = 0)
+			break ;
+	}
+	// if (context.current_state != core_state)
+		// ошибка либо кавычки не закрыты или еще че
+	free(context.buffer);
+	return (result);
 }
