674afec49061e0337c90bd869b52e088181f265e
hgrampa <hgrampa21@gmail.com>
2021-04-15 14:25:11 +0300

    Fix strdubs with empty strings

--- libft/sources/ft_strings/ft_strdup.c
+++ libft/sources/ft_strings/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hgrampa <hgrampa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 10:44:33 by hgrampa           #+#    #+#             */
-/*   Updated: 2020/11/27 15:18:21 by hgrampa          ###   ########.fr       */
+/*   Updated: 2021/04/15 14:24:12 by hgrampa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strdup(const char *s1)
 	char	*result;
 
 	len = ft_strlen(s1);
-	result = (char *)malloc(sizeof(char) * len + 1);
+	result = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!result)
 		return (NULL);
 	ft_strlcpy(result, s1, ++len);
--- libft/sources/ft_strings/ft_strndup.c
+++ libft/sources/ft_strings/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: hgrampa <hgrampa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 18:49:30 by hgrampa           #+#    #+#             */
-/*   Updated: 2021/04/13 18:49:55 by hgrampa          ###   ########.fr       */
+/*   Updated: 2021/04/15 14:24:23 by hgrampa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strndup(const char *s1, int len)
 {
 	char	*result;
 
-	result = (char *)malloc(sizeof(char) * len + 1);
+	result = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!result)
 		return (NULL);
 	ft_strlcpy(result, s1, ++len);
