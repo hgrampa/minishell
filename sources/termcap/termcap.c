0abe97de763010a42f79b527f2fccf5f20fd0a05
hgrampa <hgrampa21@gmail.com>
2021-04-18 18:42:23 +0300

    Add termcap modules

--- sources/termcap/termcap.c
+++ sources/termcap/termcap.c
@@ -6,23 +6,34 @@
 /*   By: hgrampa <hgrampa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 11:30:03 by hgrampa           #+#    #+#             */
-/*   Updated: 2021/04/18 13:19:16 by hgrampa          ###   ########.fr       */
+/*   Updated: 2021/04/18 18:41:31 by hgrampa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <termcap.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <termios.h>
+#include <unistd.h>
+#include <term.h>
+// #include <curses.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include "libft.h"
+
 
 // static char term_buffer [2048]; 
 
-typedef struct	s_termsettings
+typedef struct	s_term
 {
-	struct termios	cur_attributes;
-	struct termios	org_attributes;
-}				t_termsettings;
+	struct termios	termios;
+	struct termios	save_termios;
+}				t_term;
+
+int		ft_putchar(int	c)
+{
+	return (write(1, &c, 1));
+}
 
 void	term_up_arrow()
 {
@@ -38,88 +49,96 @@ void	term_dw_arrow()
 	// Вставляем пред строку истории
 }
 
-
 void	term_backspace()
 {
-	// Получить ширину окна
-	// Получить ширину окна что была раньше
-	// Получить кол считанных символов (до \n)
-	
-	// Узнать тек строку ((index + lable_len) / wigth)
-	// Узнать тек колонку ((index + lable_len) % wigth)
-
-	// Если тек колонка - 0 && тек строка > 0
-	//  tputs(tgetstr("up", &buff), 1, ft_putchar);
-	//  tputs(tgoto(tgetstr("ch", NULL), 0, wight (wight-1), 1, ft_putchar)
-	//  tputs(tgetstr("dc", &buff), 1, ft_putchar);
-	// Иначе
-	// 	tputs(tgetstr("le", &buff), 1, ft_putchar);
-	//  tputs(tgetstr("dc", &buff), 1, ft_putchar);
-
-
+	tputs(cursor_left, 1, ft_putchar);
+	tputs(clr_eos, 1, ft_putchar);
 }
 
-// Считать ввод пользователя
-// Начти там УС
-// 	Отработать / Удаляем УС 
-
-
-
-
-void input_reset_mode(t_termsettings *term)
+int		term_reset_mode(t_term *term)
 {
-	tcsetattr(0, TCSANOW, &term->org_attributes);
+	if (tcsetattr(0, TCSANOW, &term->save_termios) == -1)
+		return (0);
+	else
+		return (1);
 }
 
-void input_set_mode(t_termsettings *term)
+int		term_set_mode(t_term *term)
 {
+	if (tcgetattr(0, &term->save_termios) == -1)
+	{
+		// perror("fdfg");
+		return (0); // TODO код возврата
+	}
+	ft_memcpy(&term->termios, &term->save_termios, sizeof(struct termios));
+	term->termios.c_lflag &= ~(ECHO|ICANON);
+	term->termios.c_cc[VMIN] = 1;
+	term->termios.c_cc[VTIME] = 0;
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &term->termios) == -1)
+	{
+		// printf("2\n");
+		return (0); // TODO код возврата
+	}
+	return (1);
+}
 
-	tcgetattr(0, &term->org_attributes);
-	memcpy(&term->cur_attributes, &term->org_attributes, sizeof(struct termios));
-	
-	/* включаем неканонический режим без эха */
-	term->cur_attributes.c_lflag &= ~(ICANON|ECHO);
-	/* считываем минимум по одному символу */
-	term->cur_attributes.c_cc[VMIN] = 1;
-	term->cur_attributes.c_cc[VTIME] = 0;
+t_term	*term_create(void)
+{
+	t_term	*term;
 
-	tcsetattr(0, TCSAFLUSH, &term->cur_attributes);
+	term = (t_term *)ft_calloc(1, sizeof(t_term));
+	if (term == NULL)
+		return (NULL);
+	return (term);
+}
+
+int		term_init(t_term *term)
+{
+	int		success;
+	char	*term_type;
+
+	term_type = getenv("TERM");
+	if (!isatty(STDIN_FILENO))
+	{
+		printf("Not a terminal\n");
+		return (0);
+	}
+	if (term_type == NULL)
+	{
+		printf("Failed to get the name of the terminal. Set environment variable TERM\n");  // TODO код возврата
+		return (0);
+	}
+	success = tgetent(NULL, term_type);
+	if (success < 1)
+	{
+   		printf("Could not access the termcap data base or terminal type is not defined.\n"); // TODO код возврата
+		return (0);
+	}
+	return(term_set_mode(term));
+}
+
+int		term_destroy(t_term *term)
+{
+	free(term);
+	return (1);
 }
 
 int main(int argc, char const *argv[])
 {
-	char			*term_type;
-	int				success;
-	char			term_buffer [2048];
-	t_termsettings	term;
+	t_term	*term;
+	char	buff[256];
+	ssize_t read_len;
 
-	char c;
-	int	top_input_index;
-
-	printf("result = %d\n", 6 % 6);
-	exit(1);
-
-	top_input_index = 0;
+	term = term_create();
+	if (!term_init(term))
+		exit(1);
 	while (21)
 	{
-		top_input_index += read(0, &c, 1);
-		if (c == '\n')
-			break;
-		write(1, &c, 1);
+		ft_bzero(buff, 256 * sizeof(char));
+		read_len = read(0, buff, 255);
+		write(1, buff, read_len);
+		// if (buff[read_len - 1] == '\n') // вообще в строке а не так но мне похуй
+		// 	break ;
 	}
-
-	term_type = getenv("TERM");
-	if (term_type == NULL)
-	{
-		printf("Failed to get the name of the terminal. Set environment variable TERM\n");
-		return (1);
-	}
-	success = tgetent(term_buffer, term_type);
-	if (success < 1)
-	{
-   		printf("Could not access the termcap data base or terminal type is not defined.\n");
-		return (1);
-	}
-
 	return 0;
 }
