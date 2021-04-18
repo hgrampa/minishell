1f6496031d8e7566159ecb86522b12a0d3e2bdc6
ssentine <gialovesu@mail.ru>
2021-04-18 15:38:01 +0300

    Add one vsrati case

--- sources/buildins/env.c
+++ sources/buildins/env.c
@@ -7,12 +7,20 @@
 int	main(int ac, char **av, char const **env)
 {
 	t_env	*environment;
+	t_pair	*path_pair;
 
 	environment = env_create(env);
+	//env_unset(environment, "PATH");
 	if (ac == 1)
-		print_represent(environment->represent);
+	{
+		path_pair = env_get_pair(environment, "PATH");
+		if (path_pair == NULL)
+			printf("bash: env: No such file or directory\n");
+		else
+			print_represent(environment->represent);
+	}	
 	else
-		printf("out of subject %s\n", av[1]); // TODO return errno
+		printf("out of subject : %s\n", av[1]); // TODO return errno
 	env_destroy(environment);
 	return (0);
 }
