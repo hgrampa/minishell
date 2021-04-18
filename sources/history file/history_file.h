252edefc72dcfeb2e67612f426e116f7177662c9
ssentine <gialovesu@mail.ru>
2021-04-18 18:04:31 +0300

    Add history file funcs

--- sources/history file/history_file.h
+++ sources/history file/history_file.h
@@ -1,33 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bi_lists.h                                         :+:      :+:    :+:   */
+/*   history_file.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ssentine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/17 18:39:03 by ssentine          #+#    #+#             */
-/*   Updated: 2021/04/17 18:39:09 by ssentine         ###   ########.fr       */
+/*   Created: 2021/04/18 15:54:11 by ssentine          #+#    #+#             */
+/*   Updated: 2021/04/18 15:54:14 by ssentine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DLISTS_H
-# define DLISTS_H
+#ifndef HISTORY_FILE_H
+# define HISTORY_FILE_H
 
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft.h"
+# include "environment.h"
+# include "dlists.h"
+# include <stdio.h>
 
-typedef struct		s_dlist
-{
-	void			*data;
-	struct s_dlist	*next;
-	struct s_dlist	*previous;
-}					t_dlist;
-
-t_dlist				*ft_dlist_new(void *data);
-void				ft_dlist_free(t_dlist **root, void (*del)(void*));
-int					ft_dlist_pull(t_dlist **root, t_dlist *new_dlist);
-void				ft_dlist_foreach(t_dlist *root, void (*f)(void *));
-int					ft_dlist_add(t_dlist **root, void *data);
+int		serialize_history(t_dlist *root);
+t_dlist	*deserialize_history(void);
 
 #endif
