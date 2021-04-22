5d73bfd41b03bcd78625251503e089d61186e9d6
hgrampa <hgrampa21@gmail.com>
2021-04-22 17:30:30 +0300

    Add some of esc state

--- sources/parser/states/pstate_esc.c
+++ sources/parser/states/pstate_esc.c
@@ -6,29 +6,55 @@
 /*   By: hgrampa <hgrampa@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 14:17:02 by hgrampa           #+#    #+#             */
-/*   Updated: 2021/04/22 14:56:09 by hgrampa          ###   ########.fr       */
+/*   Updated: 2021/04/22 17:29:37 by hgrampa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "parser.h"
 
+# define _PRS_ESC_CORE_CAHRS "\"\';|><$ \t"
+# define _PRS_ESC_WQUOTES_CAHRS "\"\'$"
+
 // Эта стаддия пока замороженна
+// TODO проверить экранирование пробела в ""
+// TODO проверить самоэкранирование
 int		pstate_esc(char **line, struct s_pcontext *context)
 {
-	char *ancor;
+	// char *ancor;
 
-	ancor = line++;
-	
-	// (*line)++; промотка этого символа очень вариативна 
-	// while (21)
-	// {
-		
-	// 	if (**line == '\0')
-	// 	{
-	// 		pcontext_end_process(context);
-	// 		return (0);
-	// 	}
-	// }
+	(*line)++;
+	if (**line == '\0') // В теории можно избавиться от якоря
+	{
+		pcontext_end_process(context);
+		return (0); // TODO возврат ошибки (что мультистрочный ввод не поддерживается)
+	}
+	if (pcontext_previous_state() == pstate_core)
+	{
+		if (ft_strchr(_PRS_ESC_CORE_CAHRS, **line) != NULL)
+		{
+			pbuffer_add_char(context, **line);
+			(*line)++;
+		}
+		else
+		{
+			// ЧТо там в главной стадии не помню
+		}
+	}
+	else
+	{
+		if (ft_strchr(_PRS_ESC_WQUOTES_CAHRS, **line) != NULL)
+		{
+			pbuffer_add_char(context, **line);
+			(*line)++;
+		}
+		else
+		{
+			pbuffer_add_char(context, "\");
+			pbuffer_add_char(context, **line);
+			(*line)++;
+		}
+	}
+	pcontext_return_state(context);
 	return (1);
 }
